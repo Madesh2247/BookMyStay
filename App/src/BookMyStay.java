483682e503680a5b48cf0b6695c5737920aa4fb6
Madesh2247 <Maddy241306@gmail.com>
2026-04-09 09:16:44 +0530

    ApplicationEntry

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,17 @@
+public class  BookMyStay {
+    public static void main(String[] args) {
+
+        System.out.println("=================================================");
+        System.out.println("        WELCOME TO HOTEL BOOKING SYSTEM          ");
+        System.out.println("=================================================\n");
+
+        System.out.println("Initializing application...");
+        System.out.println("Loading modules...");
+        System.out.println("Setting up environment...\n");
+
+        System.out.println("System started successfully!");
+        System.out.println("Application is ready to use.\n");
+
+        System.out.println("=================================================");
+    }
+}
